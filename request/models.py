7a60e8c86c173ed7880ab3649ba6fc6ab85eb449
SiJiL82 <sven.lowry@gmail.com>
2022-04-24 12:43:42 +0100

    Add Request model

--- request/models.py
+++ request/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Request(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    requester = models.ForeignKey(User, on_delete=models.CASCADE, related_name="requests")
+    requested_item = models.CharField(max_length=50)
+    details = models.TextField()
+    required_date = models.DateField()
+    required_duration = models.IntegerField(default=1)
+    processed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+    
+    def __str__(self):
+        return f"{self.requester} requested {self.requested_item}"
