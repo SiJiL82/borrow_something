9d0e2388bbd7c26d08072251dbb3632ffb0b1c2e
SiJiL82 <sven.lowry@gmail.com>
2022-04-24 17:12:41 +0100

    Add cancel request to urls

--- request/urls.py
+++ request/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.RequestList.as_view(), name='home'),
     path('request/', views.NewRequest.as_view(), name='new_request'),
     path('myrequests/', views.MyRequestList.as_view(), name='my_requests'),
+    path('cancel/<slug:slug>', views.CancelBorrowRequest.as_view(), name='cancel_request'),
     path('<slug:slug>/', views.RequestDetail.as_view(), name='request_detail'),
 ]
