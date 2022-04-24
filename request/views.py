1e688fb63c80032ba42436d196749e8b60da64a8
SiJiL82 <sven.lowry@gmail.com>
2022-04-24 12:47:57 +0100

    Add index page view of active requests

--- request/views.py
+++ request/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Request
 
 # Create your views here.
+class RequestList(generic.ListView):
+    model = Request
+    queryset = Request.objects.filter(active=True).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
