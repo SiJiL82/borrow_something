95a84759b005aab9b4cb74a0606f76543f1acf98
SiJiL82 <sven.lowry@gmail.com>
2022-04-24 13:39:57 +0100

    Set form labels

--- request/forms.py
+++ request/forms.py
@@ -3,12 +3,12 @@ from django import forms
 
 class RequestForm(forms.ModelForm):
     class Meta:
-        model: Request
+        model = Request
         fields = ('requested_item', 'details', 'required_date', 'required_duration')
     
     def __init__(self, *args, **kwargs):
         super(RequestForm, self).__init__(*args, **kwargs)
-        self.fields['requested_item'].label = ''
-        self.fields['details'].label = ''
-        self.fields['required_date'].label = ''
-        self.fields['required_duration'].label = ''
+        self.fields['requested_item'].label = 'I would like to borrow:'
+        self.fields['details'].label = 'More details:'
+        self.fields['required_date'].label = 'On this day:'
+        self.fields['required_duration'].label = 'For this many days:'
