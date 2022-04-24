24c34d65c3331b66b713656522f03a27c34f211c
SiJiL82 <sven.lowry@gmail.com>
2022-04-24 16:30:14 +0100

    Reorder url paths

--- request/urls.py
+++ request/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RequestList.as_view(), name='home'),
-    path('request/', views.Request.as_view(), name='new_request'),
+    path('request/', views.NewRequest.as_view(), name='new_request'),
+    path('myrequests/', views.MyRequestList.as_view(), name='my_requests'),
     path('<slug:slug>/', views.RequestDetail.as_view(), name='request_detail'),
 ]
