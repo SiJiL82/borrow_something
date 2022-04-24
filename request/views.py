0dfd7665db48cdec75f5b06b22da9aaa887fc71d
SiJiL82 <sven.lowry@gmail.com>
2022-04-24 14:17:57 +0100

    Add messages and slug to post request

--- request/views.py
+++ request/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic, View
 from .models import Request
 from .forms import RequestForm
+from django.contrib import messages
+from django.utils.text import slugify
 
 # Create your views here.
 class RequestList(generic.ListView):
@@ -21,17 +23,21 @@ class Request(View):
         )
 
     def post(self, request, *args, **kwargs):
-        request_form = RequestForm(data=request.post)
+        request_form = RequestForm(data=request.POST)
         if request_form.is_valid():
-            request_form.instance.requester = request.user.username
+            request_form.instance.requester = request.user
+            slug_str = request.user.username + '-' + request_form.instance.requested_item + '-' + request_form.instance.required_date.strftime("%d%m%Y")
+            request_form.instance.slug = slugify(slug_str)
             new_request = request_form.save()
+            messages.add_message(request, messages.SUCCESS, 'Request Created!')
         else:
             request_form = RequestForm()
+            messages.add_message(request, messages.ERROR, 'Request Failed')
 
         return render(
             request,
-            "request_detail.html",
+            "new_request.html",
             {
-                "request": new_request
-            },
+                "request_form": RequestForm()
+            }
         )
